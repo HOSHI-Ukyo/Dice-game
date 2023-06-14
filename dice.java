1dbcebbb1782cd32d8ad7de69a873cf4781aa19e
HOSHI Ukyo <s1300187@u-aizu.ac.jp>
2023-06-14 19:07:14 +0900

    Won/lost message added

--- dice.java
+++ dice.java
@@ -8,5 +8,8 @@ class dice{
     int num2 = r.nextInt(6)+1;
     System.out.println("Die 1: "+num1);
     System.out.println("Die 2: "+num2);
+    System.out.print("you"+" ");
+    if(num1+num2>7){System.out.println("won!");}
+    else{ System.out.println("lost!");}
     }
 }
