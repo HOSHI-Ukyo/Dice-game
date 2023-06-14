17a9f3407274374b896ca1709ad2234d5fdc7605
HOSHI Ukyo <s1300187@u-aizu.ac.jp>
2023-06-14 19:04:28 +0900

    Greetings added

--- dice.java
+++ dice.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 class dice{
     public static void main(String[] args){
-    System.out.println("Rolling the dice...");
-    Random r = new Random();
-    int num1 = r.nextInt(6)+1;
-    int num2 = r.nextInt(6)+1;
-    System.out.println("Die 1: "+num1);
-    System.out.println("Die 2: "+num2);
+	System.out.println("What is your name?");
+	Scanner sc = new Scanner(System.in);
+	String a = sc.next();
+	System.out.println("Hello, "+a);
+	System.out.println("Rolling the dice...");
+	Random r = new Random();
+	int num1 = r.nextInt(6)+1;
+	int num2 = r.nextInt(6)+1;
+	System.out.println("Die 1: "+num1);
+	System.out.println("Die 2: "+num2);
     }
 }
