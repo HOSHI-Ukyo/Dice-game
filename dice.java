4640e43bd6bed367a9e4e8947b3093fb10d36f30
HOSHI Ukyo <s1300187@u-aizu.ac.jp>
2023-06-14 19:02:10 +0900

    Dice game: initial revision

---
+++ dice.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Random;
+class dice{
+    public static void main(String[] args){
+    System.out.println("Rolling the dice...");
+    Random r = new Random();
+    int num1 = r.nextInt(6)+1;
+    int num2 = r.nextInt(6)+1;
+    System.out.println("Die 1: "+num1);
+    System.out.println("Die 2: "+num2);
+    }
+}
